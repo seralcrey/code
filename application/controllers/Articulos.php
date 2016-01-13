ce5a9da2d940d0eed5b771ba299953b1f8aa54f2
Sergio Alcántara <sergio.alcantara.1992@gmail.com>
2016-01-13 20:58:32 +0100

    13/01/2016 Se termina el borrar (de momento)

--- application/controllers/Articulos.php
+++ application/controllers/Articulos.php
@@ -1,24 +1,42 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
 
-class Articulos extends CI_Controller
-{
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-    public function index()
-    {
+class Articulos extends CI_Controller {
+
+    public function index() {
         $data['filas'] = $this->db->query('select * from articulos ')->result_array();
         $this->load->view('articulos/index', $data);
     }
 
-    public function borrar($id)
-    {
-        $data['id'] = $id;
-        $this->load->view('articulos/borrar',$data);
+    public function borrar($id = NULL) {
+        if ($this->input->post('borrar') !== NULL) {
+            $id = $this->input->post('id');
+            if ($id !== NULL) {
+                $res = $this->db->query('delete from articulos where id = ?', array($id));
+            }
+            redirect('articulos/index');
+        }
+        else 
+        {
+            if ($id === NULL)
+            {
+                redirect('articulos/index');
+            } 
+            else 
+            {
+                $res = $this->db->query('select * from articulos where id =?', array($id));
+                if ($res->num_rows() === 0){
+                    redirect('articulos/index');
+                }
+                else 
+                {
+                    $data= $res->row_array();
+                    $this->load->view('articulos/borrar', $data);
+                }
+                
+            }
+        }
     }
-    
-    public function hacer_borrado()
-    {
-        $id = $this->input->post('id');
-        $res = $this->db->query('delete from articulos where id = ?', array($id));
-        redirect('articulos/index');
-    }
+
 }
--- application/views/articulos/borrar.php
+++ application/views/articulos/borrar.php
@@ -5,10 +5,12 @@
         <title>Borrar Artículos</title>
     </head>
     <body>
-        <h3>¿Seguro que desea borrar el artículo?</h3>
-        <?= form_open('articulos/hacer_borrado') ?>
+        <h3>¿Seguro que desea borrar el siguiente artículo? </h3>
+        <p>(<?= $codigo ?>) => <?= $nombre ?></p>
+        <?= form_open('articulos/borrar') ?>
             <?= form_hidden('id',$id)?>
             <?= form_submit('borrar', 'Sí')?>
+            <?= anchor('articulos/index' ,form_button('no', 'No'))?>
         <?= form_close()?>
     </body>
 </html>
